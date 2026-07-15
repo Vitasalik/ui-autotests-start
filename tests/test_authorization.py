ca60d3f1276de24a18cef85d43f889c76a66478d
RUKOVV <vitalya_kovalevs@mail.ru>
2026-07-15 15:47:22 +0300

    step 5.6

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -8,9 +8,10 @@ from pages.authorization_page import AuthorizationPage
 @pytest.mark.ui
 def test_authorization_wrong(user_data_wrong,
                              authorization_page: AuthorizationPage,
-                             dashboard_page: DashboardPage):
+                             dashboard_page: DashboardPage) -> None:
 
     authorization_page.open_page()
+    authorization_page.print_log_response()
     authorization_page.check_visible_authorization_form()
     authorization_page.fill_authorization_form(email=user_data_wrong['email'],
                                                password=user_data_wrong['password'])
