63cc191a52198639060802d054d9c100467ca99a
RUKOVV <vitalya_kovalevs@mail.ru>
2026-07-27 22:45:05 +0300

    Step 7.2

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,5 +1,7 @@
 import pytest
 from playwright.sync_api import Page
+
+from pages.courses_list_page import CoursesListPage
 from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
@@ -9,6 +11,7 @@ from pages.registration_page import RegistrationPage
 def registration_page(page: Page) -> RegistrationPage:
     return RegistrationPage(page)
 
+
 @pytest.fixture
 def dashboard_page(page: Page) -> DashboardPage:
     return DashboardPage(page)
@@ -17,3 +20,8 @@ def dashboard_page(page: Page) -> DashboardPage:
 @pytest.fixture
 def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
+
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
---
+++ pages/courses_list_page.py
@@ -0,0 +1,77 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+from pages.base_page import BasePage
+
+
+class CoursesListPage(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        #Заголовок и кнопка создания курса
+        self.courses_title = page.get_by_test_id("courses-list-toolbar-title-text")
+        self.create_course_button = page.get_by_test_id("courses-list-toolbar-create-course-button")
+
+        #Пустой блок при отсутствии курсов
+        self.empty_list_icon = page.get_by_test_id("courses-list-empty-view-icon")
+        self.empty_list_title = page.get_by_test_id("courses-list-empty-view-title-text")
+        self.empty_list_description = page.get_by_test_id("courses-list-empty-view-description-text")
+
+        #Карточка курса
+        self.course_title = page.get_by_test_id("course-widget-title-text")
+        self.course_image = page.get_by_test_id("course-preview-image")
+        self.course_max_score = page.get_by_test_id("course-max-score-info-row-view-text")
+        self.course_min_score = page.get_by_test_id("course-min-score-info-row-view-text")
+        self.course_estimate_time = page.get_by_test_id("course-estimated-time-info-row-view-text")
+
+        #Кнопки взаимодействия с карточкой курса
+        self.course_view_menu_button = page.get_by_test_id("course-view-menu-button")
+        self.course_edit = page.get_by_test_id("course-view-edit-menu-item-text")
+        self.course_delete = page.get_by_test_id("course-view-delete-menu-item-text")
+
+    def check_visible_courses_title(self) -> None:
+        expect(self.courses_title).to_be_visible()
+        expect(self.courses_title).to_have_text("Courses")
+
+    def check_visible_create_course_button(self) -> None:
+        expect(self.create_course_button).to_be_visible()
+
+    def click_create_course_button(self) -> None:
+        self.create_course_button.click()
+
+    def check_visible_empty_list(self) -> None:
+        expect(self.empty_list_icon).to_be_visible()
+
+        expect(self.empty_list_title).to_be_visible()
+        expect(self.empty_list_title).to_have_text("There is no results")
+
+        expect(self.empty_list_description).to_be_visible()
+        expect(self.empty_list_description).to_have_text("Results from the load test pipeline will be displayed here")
+
+    def check_visible_course_card(self, index: int, title: str, max_score: str, min_score: str,
+                                  estimate_time: str) -> None:
+        expect(self.courses_title.nth(index)).to_be_visible()
+        expect(self.courses_title.nth(index)).to_have_text(title)
+
+        expect(self.course_image.nth(index)).to_be_visible()
+
+        expect(self.course_max_score.nth(index)).to_be_visible()
+        expect(self.course_max_score.nth(index)).to_have_text(f"Max score: {max_score}")
+
+        expect(self.course_min_score.nth(index)).to_be_visible()
+        expect(self.course_min_score.nth(index)).to_have_text(f"Min score: {min_score}")
+
+        expect(self.course_estimate_time.nth(index)).to_be_visible()
+        expect(self.course_estimate_time.nth(index)).to_have_text(f"Estimated time: {estimate_time}")
+
+    def click_edit_course(self, index: int) -> None:
+        self.course_view_menu_button.nth(index).click()
+
+        expect(self.course_edit.nth(index)).to_be_visible()
+        self.course_edit.nth(index).click()
+
+    def click_delete_course(self, index: int) -> None:
+        self.course_view_menu_button.nth(index).click()
+
+        expect(self.course_delete.nth(index)).to_be_visible()
+        self.course_delete.nth(index).click()
