e79fa8d68dbd138d0cd43b2cc43a9ff1fdf439e2
RUKOVV <vitalya_kovalevs@mail.ru>
2026-08-07 14:25:05 +0300

    step 7.4 Refactoring

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,7 +1,10 @@
 import pytest
 from playwright.sync_api import Page, expect
 
+from components.courses.course_view_component import CourseViewComponent
+from components.courses.courses_list_toolbar_view_component import CoursesListToolbarViewComponent
 from components.navigation.sidebar_component import SidebarComponent
+from components.views.empty_view_component import EmptyViewComponent
 from pages.base_page import BasePage
 from components.navigation.navbar_component import NavbarComponent
 
@@ -10,76 +13,26 @@ class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        #Горизонтальный navbar
+        # Горизонтальный navbar
         self.navbar = NavbarComponent(page)
 
-        #Вертикальный sidebar
+        # Вертикальный sidebar
         self.sidebar = SidebarComponent(page)
 
-        #Заголовок и кнопка создания курса
-        self.courses_title = page.get_by_test_id("courses-list-toolbar-title-text")
-        self.create_course_button = page.get_by_test_id("courses-list-toolbar-create-course-button")
+        # Пустой блок при отсутствии курсов
+        self.empty_view = EmptyViewComponent(page, "courses-list")
 
-        #Пустой блок при отсутствии курсов
-        self.empty_list_icon = page.get_by_test_id("courses-list-empty-view-icon")
-        self.empty_list_title = page.get_by_test_id("courses-list-empty-view-title-text")
-        self.empty_list_description = page.get_by_test_id("courses-list-empty-view-description-text")
+        # Заголовок и кнопка создания курса
+        self.toolbar_view = CoursesListToolbarViewComponent(page)
 
-        #Карточка курса
-        self.course_title = page.get_by_test_id("course-widget-title-text")
-        self.course_image = page.get_by_test_id("course-preview-image")
-        self.course_max_score = page.get_by_test_id("course-max-score-info-row-view-text")
-        self.course_min_score = page.get_by_test_id("course-min-score-info-row-view-text")
-        self.course_estimate_time = page.get_by_test_id("course-estimated-time-info-row-view-text")
+        # Карточка курса
+        self.course_view = CourseViewComponent(page)
 
-        #Кнопки взаимодействия с карточкой курса
-        self.course_view_menu_button = page.get_by_test_id("course-view-menu-button")
-        self.course_edit = page.get_by_test_id("course-view-edit-menu-item-text")
-        self.course_delete = page.get_by_test_id("course-view-delete-menu-item-text")
-
-    def check_visible_courses_title(self) -> None:
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text("Courses")
-
-    def check_visible_create_course_button(self) -> None:
-        expect(self.create_course_button).to_be_visible()
-
-    def click_create_course_button(self) -> None:
-        self.create_course_button.click()
 
     def check_visible_empty_list(self) -> None:
-        expect(self.empty_list_icon).to_be_visible()
+        self.empty_view.check_visible(
+            title="There is no results",
+            description="Results from the load test pipeline will be displayed here"
+        )
 
-        expect(self.empty_list_title).to_be_visible()
-        expect(self.empty_list_title).to_have_text("There is no results")
 
-        expect(self.empty_list_description).to_be_visible()
-        expect(self.empty_list_description).to_have_text("Results from the load test pipeline will be displayed here")
-
-    def check_visible_course_card(self, index: int, title: str, max_score: str, min_score: str,
-                                  estimate_time: str) -> None:
-        expect(self.course_title.nth(index)).to_be_visible()
-        expect(self.course_title.nth(index)).to_have_text(title)
-
-        expect(self.course_image.nth(index)).to_be_visible()
-
-        expect(self.course_max_score.nth(index)).to_be_visible()
-        expect(self.course_max_score.nth(index)).to_have_text(f"Max score: {max_score}")
-
-        expect(self.course_min_score.nth(index)).to_be_visible()
-        expect(self.course_min_score.nth(index)).to_have_text(f"Min score: {min_score}")
-
-        expect(self.course_estimate_time.nth(index)).to_be_visible()
-        expect(self.course_estimate_time.nth(index)).to_have_text(f"Estimated time: {estimate_time}")
-
-    def click_edit_course(self, index: int) -> None:
-        self.course_view_menu_button.nth(index).click()
-
-        expect(self.course_edit.nth(index)).to_be_visible()
-        self.course_edit.nth(index).click()
-
-    def click_delete_course(self, index: int) -> None:
-        self.course_view_menu_button.nth(index).click()
-
-        expect(self.course_delete.nth(index)).to_be_visible()
-        self.course_delete.nth(index).click()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -13,36 +13,34 @@ def test_empty_courses_list(courses_list_page_with_state: CoursesListPage):
     courses_list_page_with_state.navbar.check_visible("username")
     courses_list_page_with_state.sidebar.check_visible()
 
-    courses_list_page_with_state.check_visible_courses_title()
-    courses_list_page_with_state.check_visible_create_course_button()
+    courses_list_page_with_state.toolbar_view.check_visible()
     courses_list_page_with_state.check_visible_empty_list()
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage) -> None:
-    create_course_page.visible("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+def test_create_course(courses_list_page_with_state: CoursesListPage, create_course_page_with_state: CreateCoursePage) -> None:
+    create_course_page_with_state.visible("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disabled_create_course_button()
+    create_course_page_with_state.check_visible_create_course_title()
 
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+    create_course_page_with_state.check_disabled_create_course_button()
 
-    create_course_page.check_visible_create_course_form("", "", "", "0", "0")
+    create_course_page_with_state.image_upload_widget.check_visible(is_image_uploaded=False)
 
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
-    create_course_page.check_visible_exercises_empty_view()
+    create_course_page_with_state.check_visible_create_course_form("", "", "", "0", "0")
 
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view(True)
+    create_course_page_with_state.check_visible_exercises_title()
+    create_course_page_with_state.check_visible_create_exercise_button()
+    create_course_page_with_state.check_visible_exercises_empty_view()
 
-    create_course_page.fill_create_course_form(
+    create_course_page_with_state.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+    create_course_page_with_state.image_upload_widget.check_visible(is_image_uploaded=True)
+
+    create_course_page_with_state.fill_create_course_form(
         "Playwright", "Playwright", "2 weeks", "100", "10")
-    create_course_page.click_create_course_button()
+    create_course_page_with_state.click_create_course_button()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
+    courses_list_page_with_state.toolbar_view.check_visible()
+    courses_list_page_with_state.course_view.check_visible(
         0, "Playwright", "100", "10", "2 weeks")
