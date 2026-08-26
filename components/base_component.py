f4279bce7151bd56b5280d76b3f64263801600be
RUKOVV <vitalya_kovalevs@mail.ru>
2026-08-26 22:36:29 +0300

    step 9.5 add logg

--- components/base_component.py
+++ components/base_component.py
@@ -1,6 +1,8 @@
-import pytest
 from typing import Pattern
 from playwright.sync_api import Page, expect
+from tools.logger import get_logger
+
+logger = get_logger("BASE_COMPONENT")
 
 
 class BaseComponent:
@@ -8,6 +10,7 @@ class BaseComponent:
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
+        logger.info(f'Checking that current url matches pattern "{expected_url.pattern}"')
         expect(self.page).to_have_url(expected_url)
 
     def refresh_page(self):
---
+++ tools/logger.py
@@ -0,0 +1,20 @@
+import logging
+
+def get_logger(name) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
+
+
+logger = get_logger("INPUT")
+logger.info('Typing to input')
+logger.info('Clicking input')
